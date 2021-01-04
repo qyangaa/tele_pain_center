00f55a240233322325b5cbe9db295d0aff81e407
arky <qyangaa@gmail.com>
2021-01-03 20:36:54 -0800

    Rendered sample filter items in providers

--- src/components/pages/providers.jsx
+++ src/components/pages/providers.jsx
@@ -4,16 +4,103 @@ import CardGroup from "react-bootstrap/CardGroup";
 import getProviders from "../../services/providerService";
 import exampleImg from "../../services/data/pictures/exampleImg.jpeg";
 import CardDeck from "../common/cardDeck";
-
+import SearchBox from "../UI/searchBox";
 class Providers extends Component {
   state = {
     providers: getProviders(),
   };
 
+  renderFilters = () => {
+    return (
+      <div id="accordion">
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+              <button
+                class="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                Collapsible Group Item #1
+              </button>
+            </h5>
+          </div>
+
+          <div
+            id="collapseOne"
+            class="collapse show"
+            aria-labelledby="headingOne"
+            data-parent="#accordion"
+          >
+            <div class="card-body">Item</div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+              <button
+                class="btn btn-link collapsed"
+                data-toggle="collapse"
+                data-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Collapsible Group Item #2
+              </button>
+            </h5>
+          </div>
+          <div
+            id="collapseTwo"
+            class="collapse"
+            aria-labelledby="headingTwo"
+            data-parent="#accordion"
+          >
+            <div class="card-body">Item</div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+              <button
+                class="btn btn-link collapsed"
+                data-toggle="collapse"
+                data-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                Collapsible Group Item #3
+              </button>
+            </h5>
+          </div>
+          <div
+            id="collapseThree"
+            class="collapse"
+            aria-labelledby="headingThree"
+            data-parent="#accordion"
+          >
+            <div class="card-body">Item</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     const { providers } = this.state;
     providers.map((provider) => (provider.photo = exampleImg));
-    return CardDeck(providers);
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-2">{this.renderFilters()}</div>
+          <div className="col-7">
+            <SearchBox />
+            {CardDeck(providers)}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
