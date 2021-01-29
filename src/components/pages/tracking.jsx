af879b88903438e26f3fc315d7edbb39946f92a4
qyangaa <qyangaa@gmail.com>
2021-01-29 15:57:38 -0800

    added human body svg to tracking page

--- src/components/pages/tracking.jsx
+++ src/components/pages/tracking.jsx
@@ -1,6 +1,16 @@
 import { Button, Alert } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
+import { ReactComponent as HumanBack } from "../../images/human_back.svg";
+import { ReactComponent as HumanFront } from "../../images/human_front.svg";
+import "./tracking.css";
 
 export default function Tracking() {
-  return <div>{console.log()}</div>;
+  return (
+    <div>
+      <div className="image-container">
+        <HumanBack className="humanImage" />
+        <HumanFront className="humanImage" />
+      </div>
+    </div>
+  );
 }
